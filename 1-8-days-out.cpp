853721f419f9cb19aa091cb04afebb2fc43ac9c9
Star ⭐ <91134654+cstarflowers@users.noreply.github.com>
2024-01-09 18:54:22 -0500

    Create 1-8-days-out.cpp

---
+++ 1-8-days-out.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int numEmployees();
+int daysMissed(int);
+double averageDays(int, int);
+
+int employees,
+	absent,
+	total;
+
+int main() {
+	total = daysMissed(numEmployees());
+	cout << endl << "There are " << employees << " employees at the company." << endl;
+	cout << "They missed a total of " << total << " days last year." << endl;
+	cout << "The average days missed by each employee is " << showpoint << setprecision(2) << averageDays(total, employees) << "." << endl;
+}
+
+int numEmployees() {
+	cout << "Please enter the number of Employees: ";
+	cin >> employees;
+	return employees;
+}
+
+int daysMissed(int employees) {
+	for (int i = 1; i <= employees; i++) {
+		cout << "Please enter the days absent for Employee " << i << ": ";
+		cin >> absent;
+		total += absent;
+	}
+	return total;
+}
+
+double averageDays(int days, int employees) {
+	double daysD = static_cast<double> (days),
+		employeesD = static_cast<double> (employees),
+		average = daysD/employeesD;
+	return average;
+}
